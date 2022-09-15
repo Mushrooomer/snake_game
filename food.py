815c92b6a853f150175ec82d6894e992e96c94f4
krystofklempir <k.klempir@gmail.com>
2022-09-15 10:15:22 +0200

    Snak Final

--- food.py
+++ food.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+import random
 
 
 class Food(Turtle):
@@ -7,6 +8,15 @@ class Food(Turtle):
         super().__init__()
         self.shape("circle")
         self.penup()
+        self.color("blue")
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
-        self.speed("fastedt")
-        random_x = random.randint
+        self.speed("fastest")
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x, random_y)
+        self.refresh()
+
+    def refresh(self):
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x, random_y)
